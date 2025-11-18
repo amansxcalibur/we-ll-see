63d106b3fd7519dd4ff3ffd4fd4c2d86f40362db
Hridesh MG <hridesh699@gmail.com>
2025-11-18 20:12:24 +0530

    refactor: Rename PostCreateView to PostsView

--- wellsee_backend/wellsee_backend/posts/views.py
+++ wellsee_backend/wellsee_backend/posts/views.py
@@ -1,18 +1,17 @@
-from rest_framework.views import APIView
-from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
+from rest_framework.views import APIView
+
 from .models import Post
 from .serializers import PostSerializer
 
-class PostCreateView(APIView):
+
+class PostsView(APIView):
     permission_classes = [IsAuthenticated]
-    
+
     def post(self, request):
-        serializer = PostSerializer(
-            data=request.data, 
-            context={'request': request}
-        )
+        serializer = PostSerializer(data=request.data, context={"request": request})
         if serializer.is_valid():
             # Associate post with authenticated user
             post = serializer.save(user=request.user)
