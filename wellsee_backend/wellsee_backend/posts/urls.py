b21f99ca44844c1a3b4d95cbe6e40fb4bf9d699d
Hridesh MG <hridesh699@gmail.com>
2025-11-18 21:53:10 +0530

    feat: Add PostDetailView for updating posts

--- wellsee_backend/wellsee_backend/posts/urls.py
+++ wellsee_backend/wellsee_backend/posts/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from .views import PostsView
+from .views import PostDetailView, PostsView
 
 app_name = "posts"
 
 urlpatterns = [
     path("", PostsView.as_view(), name="post-create"),
+    path("<int:pk>/", PostDetailView.as_view(), name="post-detail"),
 ]
--- wellsee_backend/wellsee_backend/posts/views.py
+++ wellsee_backend/wellsee_backend/posts/views.py
@@ -1,3 +1,4 @@
+from django.http import Http404
 from rest_framework import status
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
@@ -22,3 +23,27 @@ class PostsView(APIView):
         posts = Post.objects.all()
         serializer = PostSerializer(posts, many=True)
         return Response(serializer.data, status=status.HTTP_200_OK)
+
+
+class PostDetailView(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def get_object(self, pk):
+        try:
+            return Post.objects.get(pk=pk)
+        except Post.DoesNotExist:
+            raise Http404
+
+    def patch(self, request, pk):
+        post = self.get_object(pk)
+        if post.user != request.user:
+            return Response(
+                {"detail": "You do not have permission to perform this action."},
+                status=status.HTTP_403_FORBIDDEN,
+            )
+
+        serializer = PostSerializer(post, data=request.data, partial=True)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
